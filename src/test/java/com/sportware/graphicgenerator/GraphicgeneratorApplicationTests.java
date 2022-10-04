48a53fd991b48b51eade6083023a4a68c7a2d534
Nikola Mirchev <nicola.mirchev@gmail.com>
2022-10-04 09:41:03 +0300

    Initial commit

---
+++ src/test/java/com/sportware/graphicgenerator/GraphicgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportware.graphicgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphicgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
