3f425c0641e55016d0aaa7a4d50a4d97acc4c785
Nikola Mirchev <nicola.mirchev@gmail.com>
2022-10-10 15:11:33 +0300

    Added header and footer and created directory structure

--- GraphicGeneratorUI/src/app/app.module.ts
+++ GraphicGeneratorUI/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgMaterialModule } from './material/ng-material/ng-material.module';
+import { HomeInfoComponent } from './views/home-info/home-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeInfoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
